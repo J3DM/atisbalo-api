07119049073fa9bcc38617e03b5c0e8622cfefe7
EnriqueRiveiro <enriqueriveiro13@gmail.com>
2020-05-24 17:41:27 +0200

    Upload iamges

--- app.js
+++ app.js
@@ -12,9 +12,7 @@ const cors = function (req, res, next) {
     'Access-Control-Allow-Headers',
     'Origin, X-Requested-With, Content-Type, Accept, Authorization'
   )
-  res.header(
-    'Access-Control-Allow-Methods', ['GET', 'POST', 'PUT', 'DELETE']
-  )
+  res.header('Access-Control-Allow-Methods', ['GET', 'POST', 'PUT', 'DELETE'])
   next()
 }
 
