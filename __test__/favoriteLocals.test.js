3eab201d0597b12c9851d340ffa73c5c02370edf
J3DM <josemdmarcos@gmail.com>
2020-06-24 15:05:30 +0200

    added further checks for the remove and add local to favorite locals

--- __test__/favoriteLocals.test.js
+++ __test__/favoriteLocals.test.js
@@ -6,6 +6,7 @@ const userEmail = 'admin3@gmail.com'
 const userPassword = 'admin'
 var accessToken = ''
 var favoriteLocalId = ''
+var numberOfFavoriteLocalsInDatabase = null
 describe('GeoLocation Locals query', () => {
   it('Try to get the list of favourite locals without beeing logged in', async (done) => {
     const res = await app.apiServer.get('/api/user/favoriteLocals')
@@ -26,16 +27,24 @@ describe('GeoLocation Locals query', () => {
     expect(res.statusCode).toEqual(200)
     favoriteLocalId = res.body[0].id
     expect(favoriteLocalId).not.toBe('')
+    numberOfFavoriteLocalsInDatabase = res.body.length
+    expect(favoriteLocalId).not.toBe(null)
     done()
   })
   it('Remove favourite local for the logged User', async (done) => {
     const res = await app.apiServer.del('/api/user/favoriteLocals/' + favoriteLocalId).set('Authorization', accessToken)
     expect(res.statusCode).toEqual(200)
+    const resCheckOperation = await app.apiServer.get('/api/user/favoriteLocals').set('Authorization', accessToken)
+    expect(resCheckOperation.statusCode).toEqual(200)
+    expect(resCheckOperation.body.length).toEqual(numberOfFavoriteLocalsInDatabase - 1)
     done()
   })
   it('Add favourite local for the logged User', async (done) => {
     const res = await app.apiServer.post('/api/user/favoriteLocals/' + favoriteLocalId).set('Authorization', accessToken)
     expect(res.statusCode).toEqual(200)
+    const resCheckOperation = await app.apiServer.get('/api/user/favoriteLocals').set('Authorization', accessToken)
+    expect(resCheckOperation.statusCode).toEqual(200)
+    expect(resCheckOperation.body.length).toEqual(numberOfFavoriteLocalsInDatabase)
     done()
   })
   it('Get all favourtite locals offers for the logged User', async (done) => {
