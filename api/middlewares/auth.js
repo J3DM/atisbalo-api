0902171c16fbe31521bbb393a2d6ecf959b71a8b
J3DM <josemdmarcos@gmail.com>
2020-07-04 18:19:15 +0200

    Added check for the user token verification to ask Redis if user is active

--- api/middlewares/auth.js
+++ api/middlewares/auth.js
@@ -1,14 +1,20 @@
 const AuthService = require('../services/auth')
+const Redis = require('../helpers/redis')
 
 module.exports = {
   verifyToken: (req, res, next) => {
     AuthService.verifyToken(req.get('Authorization'))
-      .then((decoded) => {
+      .then(async (decoded) => {
         if (!decoded) {
           return res.status(401).json('Error when decoded the token')
         }
         req.user = decoded.user
-        next()
+        const userLogged = await Redis.existsUserData(req.user.id)
+        if (userLogged) {
+          next()
+        } else {
+          return res.status(401).json('User logged out')
+        }
       })
       .catch((err) => {
         return res.status(401).json(err)
